2e0a7daedff3d6e20284554057b246ab9cadb474
kyleyiyang <kyleyiyang@gmail.com>
2020-10-15 16:53:25 -0400

    Update isToeplitzMatrix.java

--- isToeplitzMatrix.java
+++ isToeplitzMatrix.java
@@ -17,3 +17,25 @@ public class Main
 	    return true;
 	}
 }
+// using array comparison
+
+import java.util.Arrays;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int[][] matrix = {{1,2,3,4},{5,1,2,3},{9,5,1,2}};
+		boolean t=isToeplitzMatrix(matrix);
+		System.out.println(t);
+	}
+	public static boolean isToeplitzMatrix(int[][] matrix) {
+	    for (int i=0;i<matrix.length-1;i++) {
+	        int[] arr1= Arrays.copyOfRange(matrix[i],0,matrix[0].length-1);
+	        int[] arr2= Arrays.copyOfRange(matrix[i+1],1,matrix[0].length);
+	    	if (!Arrays.equals(arr1,arr2)) {
+			return false;
+	    	}
+	    }
+	    return true;
+	}
+}
